e88ec8d2fd2be791e1af53a58411df63164e606e
한 <hih458@naver.com>
2023-11-07 00:53:52 +0900

    add serchBar 생성

--- APIRxPractice/ViewControllers/MovieSearchViewController.swift
+++ APIRxPractice/ViewControllers/MovieSearchViewController.swift
@@ -11,16 +11,35 @@ import RxCocoa
 
 class MovieSearchViewController: UIViewController {
 
-    
+    let searchBar = {
+        let search = UISearchBar()
+        search.placeholder = "영화를 검색해 주세요."
+        return search
+    }()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        setUI()
     }
 
+    func setUI() {
+        [searchBar].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            // MARK: - 서치바 레이아웃
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            
 
+        ])
+    }
 }
 
